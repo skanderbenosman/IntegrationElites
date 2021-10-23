93749a702446bc2aa6464c4648bec3547ba46ed7
Skander ben osman <mohamedskander.benosman@esprit.tn>
2021-10-23 13:16:16 +0100

    skander1234

--- src/main/java/tn/esprit/spring/repository/ContratRepository.java
+++ src/main/java/tn/esprit/spring/repository/ContratRepository.java
@@ -1,5 +1,11 @@
 package tn.esprit.spring.repository;
 
-public class ContratRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import tn.esprit.spring.entities.Contrat;
+
+@Repository
+public interface ContratRepository extends  CrudRepository<Contrat, Long> {
 
 }
