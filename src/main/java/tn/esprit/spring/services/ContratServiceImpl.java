1316ae7d9d863aa10f7693e6f165804d0ff2ce85
Mohamed skander ben osman <mohamedskander.benosman@esprit.tn>
2021-10-27 18:28:00 +0100

    test2

---
+++ src/main/java/tn/esprit/spring/services/ContratServiceImpl.java
@@ -0,0 +1,69 @@
+package tn.esprit.spring.services;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tn.esprit.spring.entities.Contrat;
+
+import tn.esprit.spring.repository.ContratRepository;
+
+@Service
+public class ContratServiceImpl implements IContratService {
+	
+	@Autowired
+	ContratRepository contratRepository;
+	private static final Logger l = LogManager.getLogger(UserServiceImpl.class);
+	@Override
+	public List<Contrat> retrieveAllContrats() {
+		List<Contrat> contrats = null; 
+		try {
+	
+			l.info("In retrieveAllUsers() : ");
+			contrats = (List<Contrat>) contratRepository.findAll();  
+			for (Contrat C : contrats) {
+				l.debug("Contrat +++ : " + C);
+			} 
+			l.info("Out of retrieveAllUsers() : ");
+		}catch (Exception e) {
+			l.error("Error in retrieveAllUsers() : " + e);
+		}
+
+		return contrats;
+	}
+
+	@Override
+	public Contrat addContrat(Contrat c) {
+		return contratRepository.save(c); 
+	}
+
+	@Override
+	public void deleteContrat(String id) {
+		 contratRepository.deleteById(Integer.parseInt(id));
+
+		
+	}
+
+	@Override
+	public Contrat updateContrat(Contrat c) {
+		return contratRepository.save(c); 
+
+	}
+
+	@Override
+	public Contrat retrieveContrat(String id) {
+		l.info("in  retrieveContrat id = " + id);
+		//User u =  userRepository.findById(Long.parseLong(id)).orElse(null);
+		//int i = 1/0; 
+		System.out.println("aaaaa");
+		Contrat c =  contratRepository.findById(Integer.parseInt(id)).orElse(null); 
+		System.out.println("skander");
+		l.info("Contrat returned : " + c);
+		
+		return c; 
+	}
+
+}
