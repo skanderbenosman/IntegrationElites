26b5c30ef8d10b14f5cb6adb9772fd566f5f5f80
Mohamed skander ben osman <mohamedskander.benosman@esprit.tn>
2021-10-30 19:12:00 +0100

    skon

--- src/main/java/tn/esprit/spring/services/UserServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/UserServiceImpl.java
@@ -63,7 +63,7 @@ public class UserServiceImpl implements IUserService {
 		l.info("in  retrieveUser id = " + id);
 		//User u =  userRepository.findById(Long.parseLong(id)).orElse(null);
 		//int i = 1/0; 
-		User u =  userRepository.findById(Long.parseLong(id)).get(); 
+		User u =  userRepository.findById(Long.parseLong(id)).orElse(null); 
 		l.info("user returned : " + u);
 		return u; 
 	}
