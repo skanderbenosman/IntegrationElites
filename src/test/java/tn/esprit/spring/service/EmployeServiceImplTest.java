9f54cb6cf6ac7c5e72c8cc9a2a31e8e3a1119f20
mohamedmselmi <mohamed.mselmi1@esprit.tn>
2021-10-31 23:32:43 +0100

    med 3

--- src/test/java/tn/esprit/spring/service/EmployeServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/EmployeServiceImplTest.java
@@ -42,7 +42,7 @@ public class EmployeServiceImplTest {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		Boolean d = false;
 	//	System.out.println("***"+uc.retrieveContrat("1") );
-		Employe u = new Employe("med", "mselmi", "mohamed.mselmi1@esprit.tn","123456", d, Role.INGENIEUR,uc.retrieveContrat("1")); 
+		Employe u = new Employe("med", "mselmi", "mohamed.mselmi1@esprit.tn","123456", d, Role.INGENIEUR,uc.retrieveContrat("2")); 
 		Employe employeAdded = us.addEmploye(u); 
 	Assert.assertEquals(u.getNom(), employeAdded.getNom());
 	}
@@ -52,9 +52,9 @@ public class EmployeServiceImplTest {
 
 	@Test
 	public void testRetrieveEmploye() {
-		Employe userRetrieved = us.retrieveEmploye("80");
+		Employe userRetrieved = us.retrieveEmploye("81");
 		System.out.println("resultat retrieve :" +userRetrieved);
-		Assert.assertEquals(80, userRetrieved.getId().longValue());
+		Assert.assertEquals(81, userRetrieved.getId().longValue());
 	}
 	
 
@@ -64,7 +64,7 @@ public class EmployeServiceImplTest {
 	public void testModifyEmploye() throws ParseException   {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		Date d = dateFormat.parse("2015-03-23");
-		Employe u = new Employe( Long. valueOf("80"),"test1", "test1", "mohamed.mselmi1@esprit.tn","20251825", true, Role.INGENIEUR,uc.retrieveContrat("3"));
+		Employe u = new Employe( Long. valueOf("81"),"test1", "test1", "mohamed.mselmi1@esprit.tn","20251825", true, Role.INGENIEUR,uc.retrieveContrat("1"));
 		Employe employeUpdated  = us.updateEmploye(u);
 		Assert.assertEquals(u.getNom(), employeUpdated.getNom());
 		Assert.assertEquals(u.getPrenom(), employeUpdated.getPrenom());
@@ -73,8 +73,8 @@ public class EmployeServiceImplTest {
 	
 	@Test
 	public void testDeleteEmploye() {
-		us.deleteEmploye("79");
-	Assert.assertNull(us.retrieveEmploye("79"));
+		us.deleteEmploye("80");
+	Assert.assertNull(us.retrieveEmploye("80"));
 	}
 
 	
